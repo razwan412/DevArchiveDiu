fe7a38704840ceab38aa176c331758ef01eb550e
razwan412 <68562513+razwan412@users.noreply.github.com>
2021-12-12 00:31:03 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,10 +36,8 @@ app.use(corsWithOptions);
 const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: [
-      'http://localhost:5000',
-      'http://localhost:3000',
-      'https://devarchivediu.netlify.app',
+     origin: [
+      '*',
     ],
     methods: ['GET', 'POST'],
     credentials: true,
