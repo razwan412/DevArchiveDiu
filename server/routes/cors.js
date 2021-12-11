c3fbd8fa349686840865ec81a796432b956eed47
razwan412 <68562513+razwan412@users.noreply.github.com>
2021-12-12 02:04:03 +0600

    Update cors.js

--- server/routes/cors.js
+++ server/routes/cors.js
@@ -1,5 +1,6 @@
 import cors from 'cors';
-const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://devarchivediu.netlify.app'];
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://devarchivediu.netlify.app',  'https://devarchivediu.vercel.app/',
+'https://devarchivediu-i9bz8j499-razwan412.vercel.app/', '*'];
 
 const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
